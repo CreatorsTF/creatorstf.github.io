c77836e243117f3baa7f8ea2ade926177eba253e
Retr0-01 <contact@retr0.dev>
2023-11-12 21:15:37 +0200

    Update astro and the deploy workflow

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,8 @@
 import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
-
 import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://creatorstf.github.io/',
-	integrations: [mdx(), sitemap()],
+	integrations: [sitemap()],
 });
